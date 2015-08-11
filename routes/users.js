d3647e42696523a84ac99a915091c4198119eb77
Sheryl Boughton <sherpbo@gmail.com>
2015-08-11 13:41:54 -0700

    make urls protocol agnostic to resolve mixed content errors

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var ig = require('instagram-node').instagram();
 var cors = require('cors');
 var mongoose = require('mongoose');
 var User = require("../models/user.js");
