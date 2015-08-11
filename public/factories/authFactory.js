4312f9db5670fecd85088407382373cffc078cc0
Sheryl Boughton <sherpbo@gmail.com>
2015-08-11 14:15:42 -0700

    add s to http request in factory

--- public/factories/authFactory.js
+++ public/factories/authFactory.js
@@ -2,10 +2,10 @@ angular.module("OurStory")
 .factory('AuthFactory', function($http){
   return {
     authUser : function (){
-      return $http.jsonp('http://the-history-of-us.herokuapp.com/authorize_user');
+      return $http.jsonp('https://the-history-of-us.herokuapp.com/authorize_user');
     },
     authUserGet : function (){
-      return $http.get('http://the-history-of-us.herokuapp.com/authorize_user');
+      return $http.get('https://the-history-of-us.herokuapp.com/authorize_user');
     }
   };
 });
